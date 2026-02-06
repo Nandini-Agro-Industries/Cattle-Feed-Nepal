8179a9de5e7dd57e04cd5c4b0ddec475badeec9e
Fossa Technology <fossatechnology123@gmail.com>
2026-02-06 19:12:11 +0545

    fix: explicit robots meta tag for indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,17 @@ export const metadata: Metadata = {
   alternates: {
     canonical: './',
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
   verification: {
     google: "12xPuXV7UMwk82v3iV7LYOV03ssuJFCA12nv91S0Zeg",
   },
